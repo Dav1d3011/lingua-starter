22765d02a354e6bf082d03a91084e479a7a2358a
David99 <ddavid3099@gmail.com>
2025-10-05 16:13:13 +0300

    chore: remove /games/flashcards and /phrases routes

--- app/games/flashcards/page.tsx
+++
@@ -1,6 +0,0 @@
-'use client';
-import { featureRegistry } from '@/core/registry';
-export default function Page() {
-    const Comp = featureRegistry.byId('flashcards')!.Page;
-    return <Comp />;
-}
